630c733efd92f59b89c4a0ce0e997c486eea81bf
Adam Howard <adam.howard.music@gmail.com>
2023-02-15 15:04:49 +0100

    Change vars to consts

--- src/classes/MainDialog.ts
+++ src/classes/MainDialog.ts
@@ -8,7 +8,7 @@ const createMainDialog = () => {
   // START UI CREATION
   // MDS
   // ===
-  var mds = new Window("palette");
+  const mds = new Window("palette");
   mds.text = "THE SORCERER'S APPRENTICE";
   mds.preferredSize.width = 510;
   mds.orientation = "column";
@@ -18,20 +18,20 @@ const createMainDialog = () => {
 
   // HEADER
   // ======
-  var header = mds.add("group", undefined, { name: "header" });
+  const header = mds.add("group", undefined, { name: "header" });
   header.orientation = "column";
   header.alignChildren = ["left", "center"];
   header.spacing = 10;
   header.margins = 0;
   header.alignment = ["fill", "top"];
 
-  var title = header.add("statictext", undefined, undefined, { name: "title" });
-  var version = project ? project.version ? project.version : "0" : "0";
+  const title = header.add("statictext", undefined, undefined, { name: "title" });
+  const version = project ? project.version ? project.version : "0" : "0";
   title.text = `The Sorcerer's Apprentice (v${version})`;
   title.alignment = ["fill", "center"];
 
-  var compTitle = header.add(ui.addTextGroup("compTitle", "Comp Title:"));
-  var outFolder = header.add(ui.addBrowseGroup("outFolder", "Output File"));
+  const compTitle = header.add(ui.addTextGroup("compTitle", "Comp Title:"));
+  const outFolder = header.add(ui.addBrowseGroup("outFolder", "Output File"));
 
   // TEMPLATE PANEL (THIS IS WHERE ALL THE GENERATED FIELDS WILL GO)
   // ========
@@ -43,40 +43,40 @@ const createMainDialog = () => {
 
   // OPTIONS
   // =======
-  var options = mds.add("group", undefined, { name: "options" });
+  const options = mds.add("group", undefined, { name: "options" });
   options.orientation = "row";
   options.alignChildren = ["left", "center"];
   options.spacing = 10;
   options.margins = 0;
 
-  var compBtn = options.add("button", undefined, undefined, { name: "compBtn" });
+  const compBtn = options.add("button", undefined, undefined, { name: "compBtn" });
   compBtn.text = "Create Comp";
 
-  var queueBtn = options.add("button", undefined, undefined, { name: "queueBtn" });
+  const queueBtn = options.add("button", undefined, undefined, { name: "queueBtn" });
   queueBtn.text = "Queue In AME";
 
-  var renderBtn = options.add("button", undefined, undefined, { name: "renderBtn" });
+  const renderBtn = options.add("button", undefined, undefined, { name: "renderBtn" });
   //renderBtn.enabled = false;
   renderBtn.text = "Render In AME";
 
   // DIVIDER
   // ===
-  var divider1 = mds.add("panel", undefined, undefined, { name: "divider1" });
+  const divider1 = mds.add("panel", undefined, undefined, { name: "divider1" });
   divider1.alignment = "fill";
 
   // STATUS
   // ====
-  var stts = mds.add("group", undefined, { name: "stts" });
+  const stts = mds.add("group", undefined, { name: "stts" });
   stts.orientation = "column";
   stts.alignChildren = ["center", "center"];
   stts.spacing = 10;
   stts.margins = 0;
   stts.alignment = ["fill", "top"];
 
-  var pbar = stts.add("progressbar", undefined, 0, 100, { name: "pbar" });
+  const pbar = stts.add("progressbar", undefined, 0, 100, { name: "pbar" });
   pbar.preferredSize.width = 480;
 
-  var status = stts.add("statictext", undefined, undefined, { name: "status" });
+  const status = stts.add("statictext", undefined, undefined, { name: "status" });
   status.text = "Status Text";
   //status.graphics.font = "dialog:24";
   status.preferredSize.width = 470;
