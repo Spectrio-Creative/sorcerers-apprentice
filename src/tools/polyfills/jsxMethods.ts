6c5edbf8263f3a9efcdcbd0d6d99c471b60ddbea
Adam Howard <adam.howard.music@gmail.com>
2023-02-16 09:08:46 +0100

    Globally extend $ and Math

--- src/tools/polyfills/jsxMethods.ts
+++ src/tools/polyfills/jsxMethods.ts
@@ -2,13 +2,23 @@ import { setUpArrayMethods } from "./array";
 import { setUpObjectMethods } from "./object";
 import { setUpStringMethods } from "./string";
 
+declare global {
+  interface $ {
+    setTimeout: (callback:()=>void, delay:number) => void;
+  }
+
+  interface Math {
+    roundTo: (float: number, dec: number) => number;
+  }
+}
+
 function extendExtendscript() {
   $.setTimeout = function (func, time) {
     $.sleep(time);
     func();
   };
 
-  Math.roundTo = (x, dec) => Math.round(x * dec) / dec;
+  Math.roundTo = (float, dec) => Math.round(float * dec) / dec;
 
   setUpArrayMethods();
   setUpStringMethods();
