86d646bd75bdc806de3fd4054f59ce3cd6e2970d
Adam Howard <adam.howard.music@gmail.com>
2023-05-04 11:44:01 +0200

    Project cofiguration

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,23 @@
 import json from "@rollup/plugin-json";
 import { getBabelOutputPlugin } from "@rollup/plugin-babel";
-import eslint from "@rollup/plugin-eslint";
+// import eslint from "@rollup/plugin-eslint";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { string } from "rollup-plugin-string";
-import cleanup from 'rollup-plugin-cleanup';
-import findUnused from "rollup-plugin-unused";
-import include from 'rollup-plugin-include';
-import typescript from '@rollup/plugin-typescript';
-
+// import cleanup from 'rollup-plugin-cleanup';
+// import findUnused from "rollup-plugin-unused";
+import include from "rollup-plugin-include";
+import typescript from "@rollup/plugin-typescript";
 
 // rollup.config.js
 export default {
   input: process.env.ENTRY || "src/main.ts",
-  output:
-    {
-      file: process.env.OUTPUT || "build/sorcerers_apprentice.jsx",
-      format: "esm",
-      sourcemap: true,
-    },
-  
+  output: {
+    file: process.env.OUTPUT || "build/sorcerers_apprentice.jsx",
+    format: "esm",
+    sourcemap: false,
+  },
+
   plugins: [
     // process.env.ENTRY
     //   ? findUnused({ exclude: ["src/**"] })
@@ -29,8 +27,11 @@ export default {
     commonjs({
       include: /node_modules/,
     }),
-    string({ include: ["actions/*", "static/*"], exclude: ["node_modules/*", "static/sorcerers_apprentice_script_2_6_2_spreadsheet.js"] }),
-    eslint({ throwOnError: true, fix: true }),
+    string({
+      include: ["actions/*", "static/*"],
+      exclude: ["node_modules/*", "static/sorcerers_apprentice_script_2_6_2_spreadsheet.js"],
+    }),
+    // eslint({ throwOnError: true, fix: true }),
     json(),
     include(),
     // cleanup(),
