32650a3ddbf7f620500e45cafecb89ec1f45f21d
Adam Howard <adam.howard.music@gmail.com>
2024-02-05 10:35:31 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { browserBtn } from "./tools/buttonFunctions";
 import { TraditionalRenderDialog } from "./classes/TraditionalRenderDialog";
 import { createSpreadsheetDialog } from "./spreadsheet/classes/SpreadsheetDialog";
 import { allEditableLayers } from "./globals/legacySupport";
+import { TemplateMain } from "./classes/template/TemplateMain";
 import { renderMain } from "./globals/project/renderMain";
 import { project } from "./globals/project/project";
 
@@ -36,5 +37,19 @@ const main = () => {
     spreadsheetDialog.show();
   }
 };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getMenuInfo() {
+  // const templates = populateTemplates(template);
+  // const tabs = [];
+  // for (let i = 0; i < templates.length; i++) {
+  //   tabs.push(poplateTabs(templates[i], template));
+  // }
+  // return { templates, tabs };
+  // const renderDialog = new TraditionalRenderDialog();
+  const template = new TemplateMain();
+
+  return JSON.stringify(template.getOverview());
+}
 
 main();
