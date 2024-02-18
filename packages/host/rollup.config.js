c9e3407080126f9c797e080209db13f1264de4dc
Adam Howard <adam.howard.music@gmail.com>
2024-02-18 21:44:12 +0100

    Update host compile to replace extendscriptr
    extendscriptr will only output IIFE code which renders the global functions un-useable by the frontend

--- packages/host/rollup.config.js
+++ packages/host/rollup.config.js
@@ -1,42 +1,51 @@
 import json from "@rollup/plugin-json";
-// import { getBabelOutputPlugin } from "@rollup/plugin-babel";
-// import eslint from "@rollup/plugin-eslint";
+import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { string } from "rollup-plugin-string";
-// import cleanup from 'rollup-plugin-cleanup';
+import cleanup from "rollup-plugin-cleanup";
 import findUnused from "rollup-plugin-unused";
 import include from "rollup-plugin-include";
 import typescript from "@rollup/plugin-typescript";
 // import stripComments from "./plugins/strip";
+import fs from "fs";
+
+const shims = fs.readFileSync(require.resolve("extendscript-es5-shim"));
 
 // rollup.config.js
 export default {
   input: process.env.ENTRY || "src/main.ts",
   output: {
-    file: process.env.OUTPUT || "build/sorcerers_apprentice.js",
+    file: process.env.OUTPUT || "build/sorcerers_apprentice.jsx",
     format: "esm",
     sourcemap: false,
+    banner: shims,
   },
-
   plugins: [
-    // process.env.ENTRY
-    //   ? findUnused({ exclude: ["src/**"] })
-    //   : findUnused({ exclude: ["src/playground.js", "**/_*.js"] }),
-    findUnused({ exclude: ["src/playground.js", "**/_*.js"] }),
-    typescript(),
+    findUnused({ exclude: ["src/playground.js", "**/_*.js", "src/playground"] }),
+    typescript({ tsconfig: "tsconfig.json", include: ["src/**/*", "../shared/**/*"]}),
     nodeResolve(),
     commonjs({
-      include: /node_modules/,
+      include: [/node_modules/, /shared/],
     }),
     string({
       include: ["actions/*", "static/*"],
       exclude: ["node_modules/*", "static/sorcerers_apprentice_script_2_6_2_spreadsheet.js"],
     }),
-    // eslint({ throwOnError: true, fix: true }),
     json(),
     include(),
-    // cleanup(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"],
+      extensions: [".ts", ".js"],
+      exclude: "node_modules/**",
+      plugins: [
+        "babel-plugin-transform-es3-member-expression-literals",
+        "babel-plugin-transform-es3-property-literals",
+        "babel-plugin-transform-es5-property-mutators",
+      ],
+    }),
+    cleanup(),
     // getBabelOutputPlugin({ presets: ["extendscript"] }),
     // stripComments(),
   ],
