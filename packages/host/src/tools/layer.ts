51370aa209dfe332aa3e906e0660822d5372962f
Adam Howard <adam.howard.music@gmail.com>
2024-02-18 21:22:31 +0100

    Update anchorpoint function

--- packages/host/src/tools/layer.ts
+++ packages/host/src/tools/layer.ts
@@ -1,6 +1,27 @@
 export interface AlignAnchorOptions {
-    offset?: [number, number];
-    rectagle?: Rect;
+  offset?: [number, number];
+  rectangle?: Rect;
+}
+
+const alignments = {
+  center: { x: -0.5, y: -0.5 },
+  right: { x: -1, y: 0 },
+  left: { x: 0, y: 0 },
+  bottom: { x: 0, y: -1 },
+  top: { x: 0, y: 0 },
+};
+
+export function getLayerDelta(layer: AVLayer, alignment: Alignment, layerRect: Rect) {
+  const [alignX, alignY] = alignment;
+  const anchorPoint = layer.anchorPoint.value;
+
+  const scale = layer.scale.value.map((s) => s / 100) as [number, number];
+  const scaledAnchorPoint = anchorPoint.map((a, i) => a * scale[i]) as [number, number] | [number, number, number];
+
+  const deltaX = (alignments[alignX].x || 0) * layerRect.width + scaledAnchorPoint[0];
+  const deltaY = (alignments[alignY].y || 0) * layerRect.height + scaledAnchorPoint[1];
+
+  return [deltaX, deltaY];
 }
 
 export function alignAnchorPoint(layer: Layer, alignment: Alignment, options: AlignAnchorOptions = {}) {
@@ -11,41 +32,65 @@ export function alignAnchorPoint(layer: Layer, alignment: Alignment, options: Al
     return;
   }
 
-  const comp = layer.containingComp;
   const layerType = parseLayerType(layer);
-
   if (!(layer instanceof TextLayer || layer instanceof AVLayer || layer instanceof ShapeLayer)) {
     alert("Cannot align anchor point of layer type: " + layerType);
     return;
   }
 
-  const layerRect: Rect = options.rectagle || layer.sourceRectAtTime(comp.time, false);
+  const childAlignment = options.rectangle ? (["left", "top"] as Alignment) : alignment;
+
+  const layerRect: Rect = getLayerRect(layer as Layer);
+  const delta = getLayerDelta(layer, childAlignment, layerRect);
+  const scale = layer.scale.value.map((s) => s / 100) as [number, number];
+
+  // If layer is text, adjust delta to account for sourceRect
+  if (layer instanceof TextLayer) {
+    const rect = layer.sourceRectAtTime(layer.containingComp.time, false);
+    delta[0] -= rect.left * scale[0];
+    delta[1] -= rect.top * scale[1];
+  }
+
+  if (options.rectangle) {
+    const rect = options.rectangle;
+    delta[0] -= rect.left - layerRect.left;
+    delta[0] += (alignments[alignX].x || 0) * rect.width;
+    delta[1] -= rect.top - layerRect.top;
+    delta[1] += (alignments[alignY].y || 0) * rect.height;
+  }
 
   const anchorPoint = layer.anchorPoint.value;
   const position = layer.position.value;
 
-  const totalWidth = layerRect.width + anchorPoint[0];
-  const totalHeight = layerRect.height + anchorPoint[1];
+  const newAnchorPoint = [anchorPoint[0] - delta[0] / scale[0], anchorPoint[1] - delta[1] / scale[1]];
+  const newPosition = [position[0] - delta[0], position[1] - delta[1]] as [number, number];
 
-  let deltaX = 0;
-
-  if (alignX === "center") deltaX = totalWidth / 2 + layerRect.left;
-  if (alignX === "right") deltaX = totalWidth + layerRect.left;
-  if (alignX === "left") deltaX = layerRect.left;
-
-  let deltaY = 0;
-
-  if (alignY === "center") deltaY = totalHeight / 2 + layerRect.top;
-  if (alignY === "bottom") deltaY = totalHeight + layerRect.top;
-  if (alignY === "top") deltaY = layerRect.top;
-
-  //   const newAnchorPoint = [anchorPoint[0] - deltaX, anchorPoint[1] - deltaY, anchorPoint[2]] as [number, number, number];
-  //   const newPosition = [position[0] - deltaX, position[1] - deltaY, position[2]] as [number, number, number];
-  const newAnchorPoint = [anchorPoint[0] + deltaX, anchorPoint[1] + deltaY] as [number, number];
-  const newPosition = [position[0] + deltaX, position[1] + deltaY] as [number, number];
-
-  layer.anchorPoint.setValue(newAnchorPoint);
+  layer.anchorPoint.setValue(newAnchorPoint as [number, number]);
   layer.position.setValue(newPosition);
+}
+
+export function getLayerRect(layer: Layer): Rect {
+  if (!(layer instanceof TextLayer || layer instanceof AVLayer || layer instanceof ShapeLayer)) return;
+
+  const comp = layer.containingComp;
+  const layerRect = layer.sourceRectAtTime(comp.time, false);
+  const anchorPoint = layer.anchorPoint.value;
+  const position = layer.position.value;
+  const scale = layer.scale.value;
+
+  const bounds = {
+    left: position[0] - anchorPoint[0] * (scale[0] / 100),
+    top: position[1] - anchorPoint[1] * (scale[1] / 100),
+    width: layerRect.width * (scale[0] / 100),
+    height: layerRect.height * (scale[1] / 100),
+  };
+
+  if (layer instanceof TextLayer) {
+    bounds.left = bounds.left + layerRect.left * (scale[0] / 100);
+    bounds.top = bounds.top + layerRect.top * (scale[1] / 100);
+  }
+
+  return bounds;
 }
 
 export function parseLayerType(layer: Layer): LayerType {
