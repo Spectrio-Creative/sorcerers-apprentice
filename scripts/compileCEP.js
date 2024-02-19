e5b20565c8b2153cdc1825df6c443c9dbf5c592f
Adam Howard <adam.howard.music@gmail.com>
2024-02-19 08:49:30 +0100

    Add current version to manifest

--- scripts/compileCEP.js
+++ scripts/compileCEP.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
 
 // Create 'dist' folder if it doesn't exist or clear it if it does
@@ -11,10 +12,19 @@ if (!fs.existsSync("dist/CSXS")) {
 
 // Move 'packages/host/dist/index.jsx' to 'dist/index.jsx'
 // fs.copyFileSync("packages/host/dist/index.jsx", "dist/index.jsx");
-fs.copyFileSync("packages/host/sorcerers_apprentice.jsx", "dist/index.jsx");
+fs.copyFileSync("packages/host/build/sorcerers_apprentice.jsx", "dist/index.jsx");
 
-// Move 'packages/host/dist/manifest.xml' to 'dist/CSXS/manifest.xml'
-fs.copyFileSync("setup/manifest.xml", "dist/CSXS/manifest.xml");
+// Read package.json and get the current version
+const packageJSON = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+// Read "setup/manifest.xml" and replace the placeholder with the current version
+let manifest = fs.readFileSync("setup/manifest.xml", "utf8");
+manifest = manifest
+  .replaceAll("{{FULL_VERSION}}", packageJSON.version)
+  .replaceAll("{{SHORT_VERSION}}", packageJSON.version.split(".").slice(0, 2).join("."));
+
+// Write the modified manifest to 'dist/CSXS/manifest.xml'
+fs.writeFileSync("dist/CSXS/manifest.xml", manifest);
 
 // Move 'packages/host/dist/setup/.debug' to 'dist/.debug'
 fs.copyFileSync("setup/.debug", "dist/.debug");
